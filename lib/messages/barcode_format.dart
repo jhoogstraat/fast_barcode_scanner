0fcb380b3ca42e7136ba2964962fa8bf0a916c89
Joshua Hoogstraat <34964599+jhoogstraat@users.noreply.github.com>
2020-07-20 12:35:58 +0200

    Adds descriptions to BarcodeFormat

--- lib/messages/barcode_format.dart
+++ lib/messages/barcode_format.dart
@@ -1,18 +1,49 @@
 enum BarcodeFormat {
-  all, // Android
-  aztec, // Android, iOS
-  code128, // Android, iOS
-  code39, // Android, iOS
-  code39mod43, // iOS
-  code93, // Android, iOS
-  codabar, // Android
-  dataMatrix, // Android, iOS
-  ean13, // Android, iOS
-  ean8, // Android, iOS
-  itf, // Android, iOS
-  pdf417, // Android, iOS
-  qr, // Android, iOS
-  upcA, // Android
-  upcE, // Android, iOS
-  interleaved, // iOS
+  /// Android
+  all,
+
+  /// Android, iOS
+  aztec,
+
+  /// Android, iOS
+  code128,
+
+  /// Android, iOS
+  code39,
+
+  /// iOS
+  code39mod43,
+
+  /// Android, iOS
+  code93,
+
+  /// Android
+  codabar,
+
+  /// Android, iOS
+  dataMatrix,
+
+  /// Android, iOS
+  ean13,
+
+  /// Android, iOS
+  ean8,
+
+  /// Android, iOS
+  itf,
+
+  /// Android, iOS
+  pdf417,
+
+  /// Android, iOS
+  qr,
+
+  /// Android
+  upcA,
+
+  /// Android, iOS
+  upcE,
+
+  /// iOS
+  interleaved,
 }
