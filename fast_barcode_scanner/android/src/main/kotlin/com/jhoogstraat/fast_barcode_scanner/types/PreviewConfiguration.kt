10d699857f2b5dfe81c5066c3d967b250e33407d
Dustin Graham <dcgraham7@gmail.com>
2022-01-08 09:23:43 -0700

    fix formatting of PreviewConfiguration.kt

--- fast_barcode_scanner/android/src/main/kotlin/com/jhoogstraat/fast_barcode_scanner/types/PreviewConfiguration.kt
+++ fast_barcode_scanner/android/src/main/kotlin/com/jhoogstraat/fast_barcode_scanner/types/PreviewConfiguration.kt
@@ -1,5 +1,14 @@
 package com.jhoogstraat.fast_barcode_scanner.types
 
-data class PreviewConfiguration(val textureId: Long, val targetRotation: Int, val width: Int, val height: Int, val analysis: String, val analysisWidth: Int, val analysisHeight: Int) {
-    fun toMap() = hashMapOf("textureId" to textureId, "targetRotation" to targetRotation, "width" to width, "height" to height, "analysis" to analysis, "analysisWidth" to analysisWidth, "analysisHeight" to analysisHeight)
+data class PreviewConfiguration(val textureId: Long, val targetRotation: Int, val width: Int,
+                                val height: Int, val analysis: String, val analysisWidth: Int,
+                                val analysisHeight: Int) {
+    fun toMap() = hashMapOf(
+            "textureId" to textureId,
+            "targetRotation" to targetRotation,
+            "width" to width,
+            "height" to height,
+            "analysis" to analysis,
+            "analysisWidth" to analysisWidth,
+            "analysisHeight" to analysisHeight)
 }
